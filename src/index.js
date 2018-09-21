f37b4954a1bc54a9cb74034a70c20f28bf28da4a
Ivan Matsur <Ivan_Matsur@epam.com>
2018-09-21 19:22:18 +0300

    Update body of "solveEquation" function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var arr = equation.split(' ');
+  var a = arr[0];
+  var b = parseInt(arr[3].toString() + arr[4].toString());
+  var c = parseInt(arr[7].toString() + arr[8].toString());
+
+  var x1 = (-1 * b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+  var x2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+
+  return [Math.round(x1), Math.round(x2)].sort((a, b) => a - b);
 }
